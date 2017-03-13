b72bb05c5c55109c056054a8b4563e05c6f852b1
joneshf <jones3.hardy@gmail.com>
2017-03-12 21:49:06 -0700

    Create `Whole` type
    Sometimes you want a number that is guaranteed to not be negative.

---
+++ Whole.elm
@@ -0,0 +1,31 @@
+module Whole exposing (Whole, fromInt, toInt)
+
+{-|
+Whole numbers are positive numbers including zero.
+Because the constructor for `Whole` is not exported, you can be
+confident that if you are using a `Whole`, then its `Whole`.
+
+@docs Whole, fromInt, toInt
+-}
+
+
+{-| -}
+type Whole
+    = Whole Int
+
+
+{-| -}
+fromInt : Int -> Maybe Whole
+fromInt x =
+    if
+        x >= 0
+    then
+        Just (Whole x)
+    else
+        Nothing
+
+
+{-| -}
+toInt : Whole -> Int
+toInt (Whole x) =
+    x
